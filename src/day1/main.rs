a90400696db8c615123988ebf6c696596211a6e6
Griffin T <a12345678910ster+git@gmail.com>
2020-12-02 22:33:29 +0800

    day 1, part 2

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -4,15 +4,17 @@ fn main() {
         .filter_map(|n| n.parse::<u32>().ok())
         .collect::<Vec<_>>();
 
-    let (x, y) = find_entries(&input, 2020);
-    println!("answer: {}", (x * y));
+    let (x, y, z) = find_entries(&input, 2020);
+    println!("answer: {}", (x * y * z));
 }
 
-fn find_entries(input: &[u32], target: u32) -> (u32, u32) {
+fn find_entries(input: &[u32], target: u32) -> (u32, u32, u32) {
     for (i, x) in input[..input.len() - 1].iter().enumerate() {
         for y in &input[i..] {
-            if x + y == target {
-                return (*x, *y);
+            for z in &input[i..] {
+                if x + y + z == target {
+                    return (*x, *y, *z);
+                }
             }
         }
     }
